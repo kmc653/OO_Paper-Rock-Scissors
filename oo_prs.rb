73b5bd1e084c34bc6d8630c66b8b250a49fbcd6a
Kevin Chang <changkaimin.big@gmail.com>
2014-08-19 21:09:35 +0800

    20140819_2

--- oo_prs.rb
+++ oo_prs.rb
@@ -1,4 +1,4 @@
-class player 
+class Player 
   attr_accessor :choice
   attr_reader :name
   
@@ -8,35 +8,75 @@ class player
 
 end
 
-class human < player
+class Human < Player
   def pick_hand
     begin
       puts "Pick one: (p, r, s)"
       self.choice = gets.chomp.downcase
-    end until prs::CHOICE.keys.include?(self.choice)
+    end until Prs::CHOICE.keys.include?(self.choice)
   end
 
   def to_s
-
+    "#{self.name} is #{Prs::CHOICE[self.choice]}"
   end
 end
 
-class computer < player
-  attr_accessor :gesture
+class Computer < Player
   
   def pick_hand
-    self.choice = prs::CHOICE.keys.sample
+    self.choice = Prs::CHOICE.keys.sample
   end
 
+  def to_s
+    "#{self.name} is #{Prs::CHOICE[self.choice]}"
+  end
 end
 
-class prs
+class Prs
   CHOICE = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
 
-  attr_accessor :human, :computer
+  attr_accessor :me, :com
 
   def initialize
-    @player = human.new
+    @me = Human.new("player")
+    @com = Computer.new("computer")
+  end
+
+  def print_winner_msg(msg)
+    case msg
+    when 'p'
+      puts "Paper wraps Rock!"
+    when 's'
+      puts "Scissors cut Paper"
+    when 'r'
+      puts "Rock breaks Scissors"
+    end
+  end 
+
+  def compare(p, c)
+    if p.choice == c.choice
+      puts "It's tie game!"
+    elsif (p.choice == 'p' && c.choice == 'r') || (p.choice == 'r' && c.choice == 's') || (p.choice == 's' && c.choice == 'p')
+      print_winner_msg(p.choice)
+      puts "You won!"
+    else
+      print_winner_msg(c.choice)
+      puts "Computer won..."
+    end
+  end
+
+  def play
+    puts "Welcome to play Paper, Rock and Scissors!"
+    begin
+      me.pick_hand
+      com.pick_hand
+      puts "#{me}, and #{com}"
+      compare(me, com)
+
+      puts "Play again? (y/n)"
+      decision = gets.chomp.downcase
+    end while decision == 'y'
   end
 end
 
+Prs.new.play
